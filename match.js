8868386752c54737f8994f067550a97165d2b738
Juliana Andrade <andrade96.juliana@gmail.com>
2026-05-18 21:56:38 -0300

    feat: Finalizando atividade do prof

--- match.js
+++ match.js
@@ -14,5 +14,10 @@ let possui = precisa.filter((item)=>{
 let qtdPossui = possui.length
 let qtd = precisa.length
 
-let porcentagem = 
+let porcentagem = ((qtdPossui / qtd) * 100)
+console.log("Itens que faltam:", falta)
+
+console.log("Itens que você já possui:", possui)
+
+console.log("Porcentagem concluída:", porcentagem + "%")
 
